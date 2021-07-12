cd49a95eee862fd8ead517b49ad680c68206c74d
ahmed-i-mokhtar <ai.mokht.eng@gmail.com>
2021-07-12 10:16:35 +0200

    advanced search for spring source files auto generated queries

---
+++ src/main/kotlin/com/sipios/springsearch/SpecificationsBuilder.kt
@@ -0,0 +1,36 @@
+package com.sipios.springsearch
+
+import com.sipios.springsearch.anotation.SearchSpec
+import javax.persistence.criteria.CriteriaBuilder
+import javax.persistence.criteria.CriteriaQuery
+import javax.persistence.criteria.Predicate
+import javax.persistence.criteria.Root
+import org.springframework.data.jpa.domain.Specification
+
+class SpecificationsBuilder<U>(searchSpecAnnotation: SearchSpec) {
+
+    private var specs: Specification<U> = NullSpecification()
+    private val parser: CriteriaParser<U> = CriteriaParser(searchSpecAnnotation)
+
+    fun withSearch(search: String): SpecificationsBuilder<U> {
+        specs = parser.parse(search)
+
+        return this
+    }
+
+    /**
+     * This function expect a search string to have been provided.
+     * The search string has been transformed into a Expression Queue with the format: [OR, value>100, AND, value<1000, label:*MONO*]
+     *
+     * @return A list of specification used to filter the underlying object using JPA specifications
+     */
+    fun build(): Specification<U> {
+        return specs
+    }
+}
+
+class NullSpecification<T> : Specification<T> {
+    override fun toPredicate(root: Root<T>, query: CriteriaQuery<*>, criteriaBuilder: CriteriaBuilder): Predicate? {
+        return null
+    }
+}
---
+++ src/main/kotlin/com/sipios/springsearch/strategies/ParsingStrategy.kt
@@ -0,0 +1,84 @@
+package com.sipios.springsearch.strategies
+
+import com.sipios.springsearch.SearchOperation
+import com.sipios.springsearch.anotation.SearchSpec
+import java.math.BigDecimal
+import java.time.Duration
+import java.time.Instant
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.util.Date
+import java.util.UUID
+import javax.persistence.criteria.CriteriaBuilder
+import javax.persistence.criteria.Path
+import javax.persistence.criteria.Predicate
+import kotlin.reflect.KClass
+import kotlin.reflect.full.isSubclassOf
+
+interface ParsingStrategy {
+    /**
+     * Method to parse the value specified to the corresponding strategy
+     *
+     * @param value Value used for the search
+     * @param fieldClass Kotlin class of the referred field
+     * @return Returns by default the value without any parsing
+     */
+    fun parse(value: String?, fieldClass: KClass<out Any>): Any? {
+        return value
+    }
+
+    /**
+     * Method to build the predicate
+     *
+     * @param builder Criteria object to build on
+     * @param path Current path for predicate
+     * @param fieldName Name of the field to be searched
+     * @param ops Search operation to use
+     * @param value Value used for the search
+     * @return Returns a Predicate instance or null if the operation was not found
+     */
+    fun buildPredicate(
+        builder: CriteriaBuilder,
+        path: Path<*>,
+        fieldName: String,
+        ops: SearchOperation?,
+        value: Any?
+    ): Predicate? {
+        return when (ops) {
+            SearchOperation.EQUALS -> builder.equal(path.get<Any>(fieldName), value)
+            SearchOperation.NOT_EQUALS -> builder.notEqual(path.get<Any>(fieldName), value)
+            SearchOperation.STARTS_WITH -> builder.like(path.get(fieldName), "$value%")
+            SearchOperation.ENDS_WITH -> builder.like(path.get(fieldName), "%$value")
+            SearchOperation.CONTAINS -> builder.like((path.get<String>(fieldName).`as`(String::class.java)), "%$value%")
+            SearchOperation.DOESNT_START_WITH -> builder.notLike(path.get(fieldName), "$value%")
+            SearchOperation.DOESNT_END_WITH -> builder.notLike(path.get(fieldName), "%$value")
+            SearchOperation.DOESNT_CONTAIN -> builder.notLike(
+                (path.get<String>(fieldName).`as`(String::class.java)),
+                "%$value%"
+            )
+            else -> null
+        }
+    }
+
+    companion object {
+        fun getStrategy(fieldClass: KClass<out Any>, searchSpecAnnotation: SearchSpec): ParsingStrategy {
+            return when {
+                fieldClass == Boolean::class -> BooleanStrategy()
+                fieldClass == Date::class -> DateStrategy()
+                fieldClass == Double::class -> DoubleStrategy()
+                fieldClass == Float::class -> FloatStrategy()
+                fieldClass == BigDecimal::class -> BigDecimalStrategy()
+                fieldClass == Instant::class -> InstantStrategy()
+                fieldClass == Int::class -> IntStrategy()
+                fieldClass.isSubclassOf(Enum::class) -> EnumStrategy()
+                fieldClass == Duration::class -> DurationStrategy()
+                fieldClass == LocalDate::class -> LocalDateStrategy()
+                fieldClass == LocalTime::class -> LocalTimeStrategy()
+                fieldClass == LocalDateTime::class -> LocalDateTimeStrategy()
+                fieldClass == UUID::class -> UUIDStrategy()
+                else -> StringStrategy(searchSpecAnnotation)
+            }
+        }
+    }
+}
